63e04d3909d90a0b6d177e50232d3da1ab45995a
Antoine Mary <amary@numericabfc.com>
2018-03-02 11:39:12 +0100

    executed build.sh

--- edge/assets/entrypoint.sh
+++ edge/assets/entrypoint.sh
@@ -7,9 +7,7 @@ if [ "x$1" = "x$APP_LOCATION" ]; then
 
     run-parts /assets/app/start
 
-    echo "Starting $APP_NAME"
     exec su-exec app $@
-    echo "$APP_NAME has stopped"
 
     run-parts /assets/app/stop
 else
